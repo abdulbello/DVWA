9fcf85efe6f55e0eab9e229e4759adee04bdb1ac
Abdulhakeem Bello <112012120+abdulbello@users.noreply.github.com>
2023-03-19 18:35:14 +0100

    Update run_sonarqube_script.sh

--- run_sonarqube_script.sh
+++ run_sonarqube_script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-java -jar /sonar-scanner-4.7.0.2747-macosx/lib/sonar-scanner-cli-4.7.0.2747.jar  -Dsonar.projectKey=UEL_Thesis   -Dsonar.sources=./backend-flask/   -Dsonar.host.url=http://172.20.10.3:9000
+java -jar /sonar-scanner-4.7.0.2747-macosx/lib/sonar-scanner-cli-4.7.0.2747.jar  -Dsonar.projectKey=UEL_Thesis \
+  -Dsonar.sources=./DVWA \
+  -Dsonar.host.url=http://192.168.168.89:9000 \
+  -Dsonar.login=sqp_a596357b8301322eacf523f516cde76db18a08f8
